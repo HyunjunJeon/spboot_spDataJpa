7b83bdcc838a6dc0ccfe78d0bec7544db4488b0d
HyunjunJeon <jeonhj920@gmail.com>
2019-12-06 13:29:20 +0900

    Title: Query Method - 2 Description:  - 반환 타입    > Spring Data JPA에서는 유연한 반환타입 지정이 가능하며 Optional을 사용하길 추천    > Mono, Flux의 Reactive 관련된 반환 타입도 존재함(R2DBC가 좀 더 발전하면 사용해볼만 할듯)  - JPA의 페이징    > offset, limit, totcnt 에 대해서 구현을 해야하고, 그것을 통해 받아온 것을 로직을 작성해서 페이징 쿼리를 만들어야함  - Oracle Dialect로 변경해서 오라클 페이징 쿼리 확인해봄(현재 업무로 진행중인 프로젝트의 페이징 쿼리와 형태가 동일함)

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
@@ -16,4 +18,30 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      */
     @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username); // setParameter 역할을 하는 @Param을 꼭 써줘야함
+
+    // * 메서드에 JPQL을 직접 작성해버림(이름없는 NamedQuery 같은 느낌...) -> 메서드명은 아무거나 써도됌
+    /*
+        Named Query랑 똑같이 App Loading 시점에 JPQL 검사를 해줌
+     */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findNandA(@Param("username") String username, @Param("age") int age);
+
+    // * JPA의 Embedded 타입도 조회가 가능함
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // * DTO로 조회
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    // * 여러개를 조회하고 싶을 때, Collection을 지원함(SQL IN 구문)
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    // * Spring Data JPA는 반환타입이 유연함
+    List<Member> findListByUsername(String username); // Collection
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalMemberByUsername(String username); // Optional 단건
+
+
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -21,6 +25,9 @@ class MemberJpaRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     @Rollback(false)
@@ -170,5 +177,107 @@ class MemberJpaRepositoryTest {
 
         assertThat(result.get(0).getUsername()).isEqualTo("aa");
 
+    }
+
+    @Test
+    @Rollback(false)
+    public void findNandA_SpringDataJPA_QueryAnnotation(){
+        Member m1 = new Member("aa", 10);
+        Member m2 = new Member("bb", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findNandA("aa", 10);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("aa");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+
+    }
+
+    @Test
+    @Rollback(false)
+    public void findMemberDto_SpringDataJPa(){
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+
+        Member m1 = new Member("aa", 10);
+        m1.changeTeam(teamA);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        assertThat(memberDto.get(0).getTeamname()).isEqualTo(teamA.getName());
+
+    }
+
+    // Collection을 넣어서 쿼리가 나가는 것 Test
+    @Test
+    public void findByNames_SpringDataJPA_Collection(){
+        Member m1 = new Member("aa", 10);
+        Member m2 = new Member("bb", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> byNames = memberRepository.findByNames(Arrays.asList("aa", "bb"));
+        byNames.forEach(System.out::println);
+    }
+
+    @Test
+    public void ManyReturnTypeTest_SpringDataJPA(){
+        Member m1 = new Member("aa", 10);
+        Member m2 = new Member("bb", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aa = memberRepository.findListByUsername("aa");
+        assertThat(aa.size()).isEqualTo(1);
+
+        // 컬렉션 반환타입에서 없는걸 조회하면 Empty Collection을 반환함
+        List<Member> asdasd = memberRepository.findListByUsername("asdasd");
+        assertThat(asdasd.size()).isEqualTo(0);
+
+        Member bb = memberRepository.findMemberByUsername("bb");
+        assertThat(bb.getUsername()).isEqualTo("bb");
+
+        // Entity 반환타입에서 없는걸 조회하면 Null 객체를 반환함
+        Member asbba = memberRepository.findMemberByUsername("asbba");
+        assertThat(asbba).isNull();
+
+        // 위의 모든 것을 해결해 줄 Java 8의 Optional <<** 이걸 써야지 **>>
+        Optional<Member> aa1 = memberRepository.findOptionalMemberByUsername("aa");
+        assertThat(aa1).isNotEmpty();
+    }
+
+    @Test
+    public void paging_JPA(){
+        // Given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+        memberJpaRepository.save(new Member("member6", 10));
+        memberJpaRepository.save(new Member("member7", 10));
+        memberJpaRepository.save(new Member("member8", 10));
+        memberJpaRepository.save(new Member("member9", 10));
+        memberJpaRepository.save(new Member("member10", 10));
+        memberJpaRepository.save(new Member("member11", 10));
+
+        int age = 10;
+        int offset = 5;
+        int limit = 7;
+
+        // When
+        List<Member> members = memberJpaRepository.findByPaging(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // Paging 계산 공식을 적용해야함..논리가 복잡해짐!
+
+        // Then
+        assertThat(members.size()).isEqualTo(6); // 6 ~ 11번까지 6개 가져옴
+        assertThat(totalCount).isEqualTo(11);
     }
 }
